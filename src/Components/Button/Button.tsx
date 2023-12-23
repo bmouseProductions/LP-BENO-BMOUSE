c2f71dcd29f8b0a447f8fc2ecc38ac1cefe7c77d
jvcorado <jhonisilva545@gmail.com>
2023-12-23 00:22:31 -0300

    alterações jv

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,7 +1,7 @@
 const ContatoButton = () => {
   return (
     <button
-      className="bg-[#33F1DD]  px-6 py-3 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+      className="bg-[#33F1DD] text-2xl uppercase font-bold w-full px-6 py-3 rounded-full focus:outline-none focus:ring focus:border-blue-300"
       style={{
         boxShadow:
           "0px 0px 12.946px 0px #03BFAC, 0px 0px 25.891px 0px #03BFAC, 0px 0px 90.619px 0px #03BFAC, 0px 0px 181.238px 0px #03BFAC, 0px 0px 310.694px 0px #03BFAC, 0px 0px 543.715px 0px #03BFAC",
--- src/Pages/carousel/Carousel.tsx
+++ src/Pages/carousel/Carousel.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import Slider from "react-slick";
+import Realidade from "../../assets/Realiade.svg";
+/* import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick-theme.css"; */
 import { useMediaQuery } from "react-responsive";
 
 const Carousel = () => {
-  const images = ["/Bmouse.png", "/Patense.png", "/Salus.png"];
+  /*   const images = ["/Bmouse.png", "/Patense.png", "/Salus.png"]; */
 
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
@@ -13,26 +13,29 @@ const Carousel = () => {
     // Se não for um dispositivo móvel, renderize as imagens lado a lado
     return (
       <>
-        <div className="border-2 border-solid rounded-full border-[#33F1DD] text-center p-2 items-center align-bottom justify-center max-w-xs  mx-auto bg-transparent w-full relative">
-          <h1 className="text-base font-light ">
-            <span>Empresas</span> que já estão usando VR:
-          </h1>
-        </div>
-        <div className="flex justify-center items-center space-x-8 mt-8">
-          {images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`Image ${index + 1}`}
-              className="max-w-full max-h-32 object-contain"
-            />
-          ))}
+        <div className="bg-stone-500 py-10">
+          {/*  <div className=" border-2 border-solid rounded-full border-[#33F1DD] text-center p-2 items-center align-bottom justify-center max-w-xs  mx-auto bg-transparent w-full relative">
+            <h1 className="text-base font-light ">
+              <span>Empresas</span> que já estão usando VR:
+            </h1>
+          </div>
+          <div className="flex justify-center items-center space-x-8 mt-8 w-full">
+            {images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Image ${index + 1}`}
+                className="max-w-full max-h-32 object-contain"
+              />
+            ))}
+          </div> */}
+          <img src={Realidade} alt="" className="mx-auto" />
         </div>
       </>
     );
   }
 
-  const settings = {
+  /*  const settings = {
     dots: true,
     infinite: true,
     speed: 500,
@@ -41,10 +44,11 @@ const Carousel = () => {
     autoplay: true,
     autoplaySpeed: 3000, // Intervalo de 3 segundos (ajuste conforme necessário)
   };
-
+ */
   return (
     <div className="">
-      <div className="border-2 border-solid rounded-full border-[#33F1DD] text-center p-2 items-center align-bottom justify-center max-w-xs  mx-auto bg-transparent w-full relative">
+      <img src={Realidade} alt="" className="mx-auto" />
+      {/* <div className="  border-2 border-solid rounded-full border-[#33F1DD] text-center p-2 items-center align-bottom justify-center max-w-xs  mx-auto bg-red-400 w-full relative">
         <h1 className="text-base font-light ">
           <span>Empresas</span> que já estão usando VR:
         </h1>
@@ -59,7 +63,7 @@ const Carousel = () => {
             />
           </div>
         ))}
-      </Slider>
+      </Slider> */}
     </div>
   );
 };
